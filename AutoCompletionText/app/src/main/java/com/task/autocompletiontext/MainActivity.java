45971741394978e8bfedd3b56b3e1b2be140ae69
Sujeet Suman <sujeetxavier793@gmail.com>
2020-04-24 17:49:15 +0530

    the Aotocomplete App is formed

--- AutoCompletionText/app/src/main/java/com/task/autocompletiontext/MainActivity.java
+++ AutoCompletionText/app/src/main/java/com/task/autocompletiontext/MainActivity.java
@@ -3,12 +3,22 @@ package com.task.autocompletiontext;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    AutoCompleteTextView autoCompleteTextView;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String [] values=getResources().getStringArray(R.array.countries);
+        ArrayAdapter <String> arrayAdapter =new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,values);
+        autoCompleteTextView=findViewById(R.id.actv);
+        autoCompleteTextView.setAdapter(arrayAdapter);
+        autoCompleteTextView.setThreshold(1);
     }
 }
